39095d666483b381ed2c746afaff81023b97ecbf
Mahdi Najjarian <mnajarian@gmail.com>
2020-01-20 13:29:36 +0200

    add app and refactor server and deploy to heroku

--- app/src/App.js
+++ app/src/App.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 import Package from './Package';
 import Detail from './Detail';
 
-const baseUrl = 'http://localhost:3001/'
+const baseUrl = '/api'
 
 const App = () => {
   const [state, setstate] = useState([])
